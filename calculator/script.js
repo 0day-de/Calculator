7cafcc5c8314d774cd4f7e7159b58b99e5d6bcfd
d3coder <150414506+0day-de@users.noreply.github.com>
2024-03-13 22:01:01 +0530

    Add files via upload

---
+++ calculator/script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+
+function msg(){
+    alert("Calculator By - Deepanshu");
+}
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "";
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error";
+    }
+}
